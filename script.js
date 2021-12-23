802e652f6875f7a99a98ca02218eea90247a3c25
randymz10 <randymz10@gmail.com>
2021-12-23 15:31:16 -0300

    Jogo quase pronto :D

--- script.js
+++ script.js
@@ -47,6 +47,13 @@ function iniciarJogo() {
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for (let i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert('Game Over D: ');
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
@@ -65,8 +72,6 @@ function iniciarJogo() {
         food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
-
-
 
     let newHead = {
         x: snakeX,
