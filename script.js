a5a8703df23d07a4f770373962c11662d57299e5
randymz10 <randymz10@gmail.com>
2021-12-23 12:06:50 -0300

    criado o background e a cobrinha

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
-snake[0] = {
-    x = 8 * box,
-    y = 8 * box
+snake[0]= {
+    x: 8 * box,
+    y: 8 * box
 }
-let direction = "right";
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -14,32 +13,12 @@ function criarBG() {
 }
 
 function criarCobrinha() {
-    for (let i = 0; i < snake.length; i++) {
+    for(i = 0; i<snake.length; i++){
         context.fillStyle = "green";
-        context.fillRect(snake[i].x, snake[i].y, box, box)
-
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-function iniciarJogo() {
-    criarBG();
-    criarCobrinha();
-    let snakeX = snake[0].x;
-    let snakeY = snake[0].y;
 
-    if (direction == "righ") snakeX += box;
-    if (direction == "left") snakeX -= box;
-    if (direction == "up") snakeY -= box;
-    if (direction == "down") snakeY += box;
-
-    snake.pop();
-
-    let newHead = {
-        x: snakeX,
-        y: snakeY
-    }
-
-    snake.unshift(newHead)
-}
-
-let jogo = setInterval(iniciarJogo, 100);
+criarBG();
+criarCobrinha();
